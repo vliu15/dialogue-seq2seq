f92efcbdd0fe9c59ca8fcde707a7dc9060401b14
Vincent Liu <vliu15@stanford.edu>
2019-01-28 13:52:46 -0800

    add download for internet argument corpus

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 # install dependencies
 pip3 install -r requirements.txt
 
+### === WMT DATA === ###
 # moses tokenizer and bleu score
 if [ ! -f tokenizer.perl ]; then 
     wget https://raw.githubusercontent.com/moses-smt/mosesdecoder/master/scripts/tokenizer/tokenizer.perl; fi
@@ -14,10 +15,10 @@ sed -i '.bak' "s/$RealBin\/..\/share\/nonbreaking_prefixes//" tokenizer.perl
 if [ ! -f multi-bleu.perl ]; then
     wget https://raw.githubusercontent.com/moses-smt/mosesdecoder/master/scripts/generic/multi-bleu.perl; fi
 
-# download data
+# download wmt data
 if [ ! -d data/multi30k ]; then
     mkdir -p data/multi30k
-    wget http://www.quest.dcs.shef.ac.uk/wmt16_files_mmt/training.tar.gz &&  tar -xf training.tar.gz -C data/multi30k && rm training.tar.gz
+    wget http://www.quest.dcs.shef.ac.uk/wmt16_files_mmt/training.tar.gz && tar -xf training.tar.gz -C data/multi30k && rm training.tar.gz
     wget http://www.quest.dcs.shef.ac.uk/wmt16_files_mmt/validation.tar.gz && tar -xf validation.tar.gz -C data/multi30k && rm validation.tar.gz
     wget http://www.quest.dcs.shef.ac.uk/wmt16_files_mmt/mmt16_task1_test.tar.gz && tar -xf mmt16_task1_test.tar.gz -C data/multi30k && rm mmt16_task1_test.tar.gz
 fi
@@ -26,3 +27,11 @@ fi
 for l in en de; do for f in data/multi30k/*.$l; do if [[ "$f" != *"test"* ]]; then sed -i '' "$ d" $f; fi;  done; done
 for l in en de; do for f in data/multi30k/*.$l; do perl tokenizer.perl -a -no-escape -l $l -q  < $f > $f.atok; done; done
 python3 preprocess.py -train_src data/multi30k/train.en.atok -train_tgt data/multi30k/train.de.atok -valid_src data/multi30k/val.en.atok -valid_tgt data/multi30k/val.de.atok -save_data data/multi30k.atok.low.pt
+
+### === ARGUMENT CORPUS DATA === ###
+# download argument corpus data
+if [ ! -d data/arg5k ]; then
+    mkdir -p data/arg5k
+    wget http://nldslab.soe.ucsc.edu/iac/v2/convinceme_no_parse_2016_05_18.sql.gz && gunzip convinceme_no_parse_2016_05_18.sql.gz -C data/arg5k && rm convinceme_no_parse_2016_05_18.sql.gz
+    wget http://nldslab.soe.ucsc.edu/iac/v2/convinceme_2016_05_18.sql.gz && gunzip convinceme_2016_05_18.sql.gz -C data/arg5k && rm convinceme_2016_05_18.sql.gz
+fi
