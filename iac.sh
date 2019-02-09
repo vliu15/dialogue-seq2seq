74c8c776b4eaa1f5f99b87ac65167853c2ef3452
Vincent Liu <36426273+vliu15@users.noreply.github.com>
2019-02-08 18:58:19 -0800

    Update iac.sh

--- iac.sh
+++ iac.sh
@@ -2,5 +2,5 @@
 
 # download iacorpus dataset
 if [ ! -d data/iac_v1.1 ]; then
-    wget http://nldslab.soe.ucsc.edu/iac/iac_v1.1.zip && unzip -C data && rm iac_v1.1.zip
+    wget http://nldslab.soe.ucsc.edu/iac/iac_v1.1.zip && unzip iac_v1.1.zip -C data && rm iac_v1.1.zip
 fi
