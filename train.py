0ef34ee0a4ac22e84916d4bacf197fe57c78e660
Vincent Liu <vliu15@stanford.edu>
2019-02-14 19:41:24 -0800

    Update default params

--- train.py
+++ train.py
@@ -223,18 +223,18 @@ def main():
 
     parser.add_argument('-epoch', type=int, default=10)
     parser.add_argument('-batch_size', type=int, default=4)
-    parser.add_argument('-lr', type=float, default=1e-4)
+    parser.add_argument('-lr', type=float, default=1e-3)
 
     #parser.add_argument('-d_word_vec', type=int, default=512)
-    parser.add_argument('-d_model', type=int, default=128)
+    parser.add_argument('-d_model', type=int, default=512)
     parser.add_argument('-d_inner_hid', type=int, default=512)
-    parser.add_argument('-d_k', type=int, default=16)
-    parser.add_argument('-d_v', type=int, default=16)
+    parser.add_argument('-d_k', type=int, default=64)
+    parser.add_argument('-d_v', type=int, default=64)
     parser.add_argument('-d_hidden', type=int, default=512)
 
     parser.add_argument('-n_head', type=int, default=8)
-    parser.add_argument('-n_layers', type=int, default=4)
-    parser.add_argument('-n_warmup_steps', type=int, default=4000)
+    parser.add_argument('-n_layers', type=int, default=6)
+    parser.add_argument('-n_warmup_steps', type=int, default=1000)
 
     parser.add_argument('-dropout', type=float, default=0.2)
     parser.add_argument('-embs_share_weight', action='store_true')
