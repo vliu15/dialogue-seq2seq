3a5530ab323aec79b9d233fb2ae5f41bcbcb4448
Vincent Jian Liu <vliu15@rice16.stanford.edu>
2019-03-08 18:30:46 -0800

    Adjust params for training to fit in mem

--- train.py
+++ train.py
@@ -270,7 +270,7 @@ def main():
     parser.add_argument('-data', required=True)
 
     parser.add_argument('-epoch', type=int, default=100)
-    parser.add_argument('-batch_size', type=int, default=4)
+    parser.add_argument('-batch_size', type=int, default=2)
     parser.add_argument('-lr', type=float, default=5e-2)
 
     parser.add_argument('-src_emb_file', type=str, default='')
@@ -285,7 +285,7 @@ def main():
 
     parser.add_argument('-n_head', type=int, default=8)
     parser.add_argument('-n_layers', type=int, default=5)
-    parser.add_argument('-n_warmup_steps', type=int, default=2000)
+    parser.add_argument('-n_warmup_steps', type=int, default=4000)
 
     parser.add_argument('-dropout', type=float, default=0.1)
     parser.add_argument('-embs_share_weight', action='store_true')
