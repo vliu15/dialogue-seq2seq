bcc56654a044c9bd77a74c1aa7fa37a381ed0b74
Vincent Liu <vliu15@stanford.edu>
2019-03-12 11:40:55 -0700

    Fix flag

--- transformer/Translator.py
+++ transformer/Translator.py
@@ -37,7 +37,7 @@ class Translator(object):
             n_layers=model_opt.n_layers,
             n_head=model_opt.n_head,
             dropout=model_opt.dropout,
-            train_for_mmi_loss=model_opt.train_for_mmi_loss,
+            train_for_mmi_loss=model_opt.loss_mmi,
             src_emb_file=model_opt.src_emb_file,
             tgt_emb_file=model_opt.tgt_emb_file)
 
